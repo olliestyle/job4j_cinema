ed504a53ff87846fcd9559d035a68117e8da0067
olliestyle <olliestyle1989@gmail.com>
2021-11-15 15:32:52 +0300

    WorkVersion1.3

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,16 @@
-CREATE TABLE account (
+
+CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     phone VARCHAR NOT NULL UNIQUE
 );
 
-CREATE TABLE ticket (
+CREATE TABLE tickets (
     id SERIAL PRIMARY KEY,
     session_id INT NOT NULL,
     row INT NOT NULL,
-    cell INT NOT NULL,
-    account_id INT NOT NULL REFERENCES account(id)
+    place INT NOT NULL,
+    account_id INT NOT NULL REFERENCES accounts(id),
+    UNIQUE (session_id, row, place)
 );
